a95d63adbe7ee5afa9c3bf8ebfd5ea426265deda
Liaomandy <106022038@live.asia.edu.tw>
2017-09-25 15:18:13 +0800

    third commit

--- src/gittest.java
+++ src/gittest.java
@@ -2,5 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("第一個java程式");
         System.out.println("第二個java程式");
+        System.out.println("第三個java程式");
     }
 }
